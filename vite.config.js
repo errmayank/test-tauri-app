0a52ae8b5102862f474731b5cc82a09ef25770bd
Mayank Verma <errmayank@gmail.com>
2024-07-25 12:09:35 +0530

    shouldn't run

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [sveltekit()],
 
